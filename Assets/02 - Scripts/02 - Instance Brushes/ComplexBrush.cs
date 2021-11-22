9d2dd21d757ccaa8e49d090b84848308f047f809
Daniil Smolyakov <danonofficial@gmail.com>
2021-11-22 13:27:56 +0100

    updated_brush

--- Assets/02 - Scripts/02 - Instance Brushes/ComplexBrush.cs
+++ Assets/02 - Scripts/02 - Instance Brushes/ComplexBrush.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
+using System;
 public class ComplexBrush : InstanceBrush
 {
     public enum TreeType
@@ -14,16 +15,18 @@ public class ComplexBrush : InstanceBrush
     public float maxSteepnes = 1.0f;
     public float broadLeafMaxHeight = 10f;
     public float broadPalmTreeMaxHeight = 20f;
+    public float minimumDistance = 1f;
     TreeType treeType = TreeType.BROADLEAF_TREE;
 
     public override void draw(float x, float z)
     {
-        float distance = Random.Range(0, radius);
-        float theta = Random.Range(0, 2 * Mathf.PI);
+        float distance = UnityEngine.Random.Range(0, radius);
+        float theta = UnityEngine.Random.Range(0, 2 * Mathf.PI);
         Vector2 resultVector = new Vector2(0, distance);
         resultVector = Quaternion.Euler(0, 0, theta) * resultVector;
-        float resultX = x + resultVector.x;
-        float resultZ = z + resultVector.y;
+        resultVector += new Vector2(x, z);
+        float resultX = resultVector.x;
+        float resultZ = resultVector.y;
         float currentHeight = terrain.get(resultX, resultZ);
         print(currentHeight);
         if (currentHeight < broadLeafMaxHeight)
@@ -39,6 +42,18 @@ public class ComplexBrush : InstanceBrush
             treeType = TreeType.BUSH;
         }
         if (maxSteepnes < terrain.getSteepness(resultX, resultZ))
+        {
+            return;
+        }
+        int amountOfObjects = terrain.getObjectCount();
+        float smallestDinstance = Single.MaxValue;
+        for(int i = 0; i < amountOfObjects; i++)
+        {
+            Vector3 position = terrain.getObjectLoc(i);
+            Vector2 planePos = new Vector2(position.x, position.z);
+            smallestDinstance = Mathf.Min(smallestDinstance, Vector2.Distance(planePos, resultVector));
+        }
+        if(smallestDinstance < minimumDistance)
         {
             return;
         }
